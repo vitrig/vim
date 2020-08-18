525328b5a1dc49c362a93c78201c5d890c4525f5
Przemyslaw Czarnota <przemyslawx.czarnota@intel.com>
2020-08-18 16:11:01 +0200

    Update 'Anonymice', 'Powerline', 'Bold', 'Italic.ttf' and others

--- install.sh
+++ install.sh
@@ -6,10 +6,6 @@ if [ "$?" -ne "0" ]; then
     exit 1;
 fi
 
-if ! "$DIR/bundle/fonts/install.sh" 2> /dev/null; then
-    echo "Cannot install powerline fonts. Vim will work without them"
-fi
-
 cp -vf "$DIR/.vimrc" ~/.vimrc
 
 true ${XDG_CONFIG_HOME:=$HOME/.config}
